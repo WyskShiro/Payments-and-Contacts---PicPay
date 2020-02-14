95334e29301075a93155111843c9cbe5d164f489
William <william@jera.com.br>
2020-02-13 22:04:43 -0400

    Setting Realm Initial setup

--- presentation/src/main/java/will/shiro/desafiopicpay/PicPayApplication.kt
+++ presentation/src/main/java/will/shiro/desafiopicpay/PicPayApplication.kt
@@ -1,16 +1,26 @@
 package will.shiro.desafiopicpay
 
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyProperties
 import androidx.multidex.MultiDexApplication
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
+import io.realm.Realm
+import io.realm.RealmConfiguration
 import will.shiro.desafiopicpay.util.di.component.DaggerAppComponent
+import java.security.KeyPairGenerator
 import javax.inject.Inject
+import javax.inject.Named
 
 class PicPayApplication : MultiDexApplication(), HasAndroidInjector {
 
     @Inject
     lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
+
+    @Inject
+    @Named("ENCRYPTION_KEY")
+    lateinit var encryptionKey: ByteArray
 
     override fun onCreate() {
         super.onCreate()
@@ -18,7 +28,17 @@ class PicPayApplication : MultiDexApplication(), HasAndroidInjector {
             .context(this)
             .build()
             .inject(this)
+        setupRealm()
     }
 
     override fun androidInjector(): AndroidInjector<Any> = dispatchingAndroidInjector
+
+    private fun setupRealm() {
+        Realm.init(applicationContext)
+        val config = RealmConfiguration.Builder()
+            .deleteRealmIfMigrationNeeded()
+            .encryptionKey(encryptionKey)
+            .build()
+        Realm.setDefaultConfiguration(config)
+    }
 }
--- presentation/src/main/java/will/shiro/desafiopicpay/util/di/module/ApiProviderModule.kt
+++ presentation/src/main/java/will/shiro/desafiopicpay/util/di/module/ApiProviderModule.kt
@@ -1,8 +1,11 @@
 package will.shiro.desafiopicpay.util.di.module
 
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyProperties
 import dagger.Module
 import dagger.Provides
 import dagger.multibindings.IntoSet
+import io.realm.Realm
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -12,6 +15,8 @@ import retrofit2.converter.gson.GsonConverterFactory
 import will.shiro.data.client.ApiClient
 import will.shiro.data.client.ApiService
 import will.shiro.desafiopicpay.BuildConfig
+import java.security.KeyPairGenerator
+import javax.inject.Named
 import javax.inject.Singleton
 
 @Module
@@ -73,5 +78,31 @@ object ApiProviderModule {
         } else {
             HttpLoggingInterceptor.Level.NONE
         }
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideRealm(): Realm {
+        return Realm.getDefaultInstance()
+    }
+
+    @Provides
+    @Named("ENCRYPTION_KEY")
+    @Singleton
+    fun provideEncryptionKey(): ByteArray {
+        val kpg: KeyPairGenerator = KeyPairGenerator.getInstance(
+            KeyProperties.KEY_ALGORITHM_EC,
+            "AndroidKeyStore"
+        )
+        val parameterSpec: KeyGenParameterSpec = KeyGenParameterSpec.Builder(
+            "AndroidKeyStore",
+            KeyProperties.PURPOSE_SIGN or KeyProperties.PURPOSE_VERIFY
+        ).run {
+            setDigests(KeyProperties.DIGEST_SHA256, KeyProperties.DIGEST_SHA512)
+            build()
+        }
+        kpg.initialize(parameterSpec)
+        return kpg.generateKeyPair().public.encoded.copyOfRange(0, 64)
     }
 }
