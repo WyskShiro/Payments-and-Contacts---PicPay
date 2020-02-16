30b568f4bf0f742187a31ded81fd17a48032f06a
William <william@jera.com.br>
2020-02-16 12:19:22 -0400

    Refactor and changing colors for edittext and textview

--- presentation/src/main/java/will/shiro/desafiopicpay/util/customview/MoneyInputLayout.kt
+++ presentation/src/main/java/will/shiro/desafiopicpay/util/customview/MoneyInputLayout.kt
@@ -8,7 +8,7 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import setClick
 import will.shiro.desafiopicpay.databinding.CustomMoneyInputViewBinding
-import will.shiro.desafiopicpay.util.watcher.SimpleTextWatcher
+import will.shiro.desafiopicpay.util.mask.MoneyMask
 
 class MoneyInputLayout @JvmOverloads constructor(
     context: Context,
@@ -31,50 +31,6 @@ class MoneyInputLayout @JvmOverloads constructor(
         editText.setClick {
             editText.setSelection(editText.text.length)
         }
-        editText.addTextChangedListener(object : SimpleTextWatcher() {
-            var oldText = editText.text.toString()
-            var isUpdating = false
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                val newText = s.toString()
-                if (isUpdating) {
-                    isUpdating = false
-                } else {
-                    var newTextWithoutSymbols = newText.replace(Regex("[^0-9]"), "")
-                    if (newText.length < 4) {
-                        newTextWithoutSymbols = "0" + newTextWithoutSymbols.substring(0 until 2)
-                        newTextWithoutSymbols = newTextWithoutSymbols.substring(0 until 1) +
-                                "," + newTextWithoutSymbols.substring(1 until newTextWithoutSymbols.length)
-                        isUpdating = true
-                        editText.setText(newTextWithoutSymbols)
-                        oldText = newTextWithoutSymbols
-                        editText.setSelection(4)
-                    } else {
-                        if (newTextWithoutSymbols.startsWith("0")) {
-                            newTextWithoutSymbols = newTextWithoutSymbols.removeRange(0, 1)
-                        }
-
-                        var i = newTextWithoutSymbols.length - 2
-                        newTextWithoutSymbols = newTextWithoutSymbols.substring(0 until i) +
-                                "," + newTextWithoutSymbols.substring(i until newTextWithoutSymbols.length)
-                        i -= 3
-                        while (i > 0) {
-                            newTextWithoutSymbols = newTextWithoutSymbols.substring(0 until i) +
-                                    "." + newTextWithoutSymbols.substring(i until newTextWithoutSymbols.length)
-                            i -= 3
-                        }
-                        isUpdating = true
-                        editText.setText(newTextWithoutSymbols)
-                        editText.setSelection(newTextWithoutSymbols.length)
-                        oldText = newTextWithoutSymbols
-                    }
-                }
-            }
-
-            private fun insertComma(text: String, position: Int): String {
-                return text.substring(0 until position) +
-                        "," + text.substring(position until text.length)
-            }
-        })
+        editText.addTextChangedListener(MoneyMask(editText, textView))
     }
 }
